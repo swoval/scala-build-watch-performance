fda2f2632aac94c2a1d82afecdf0cbed79630ccd
Ethan Atkins <contact@ethanatkins.com>
2020-09-26 11:55:55 -0700

    Add sbt.ci=true

--- src/main/java/build/performance/Main.java
+++ src/main/java/build/performance/Main.java
@@ -179,6 +179,7 @@ public class Main {
                   "-XX:+UseParallelGC",
                   "-Dsbt.supershell=never",
                   "-Dsbt.color=" + color,
+                  "-Dsbt.ci=true",
                   "-jar",
                   binary,
                   "~test");
